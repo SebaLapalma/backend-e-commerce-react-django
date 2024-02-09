d1e4575db2b5853f73f12804b2b59d4c51073472
SebaLapalma <sebastianlapalma@Sebastians-MacBook-Pro.local>
2024-02-09 11:54:26 -0300

    changing path for STATIC_ROOT

--- backend/settings.py
+++ backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -186,7 +186,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = 'static/assets'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_ROOT = 'static/images'
 
 # Default primary key field type

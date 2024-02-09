6ef686f641abed9c77065f469f1417d8d45fc7b9
SebaLapalma <sebastianlapalma@Sebastians-MacBook-Pro.local>
2024-02-09 10:28:02 -0300

    postgres db and static root changes

--- backend/settings.py
+++ backend/settings.py
@@ -127,11 +127,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
+if debut:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'backendDB',
+        'USER': 'sebastian',
+        'PASSWORD': '38543890',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -169,6 +180,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'

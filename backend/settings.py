c4762e1077e571b2f1147740efc509a01fdfb87b
SebaLapalma <sebastianlapalma@Sebastians-MacBook-Pro.local>
2024-02-09 12:35:47 -0300

    set DEBUG to False

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+wp7_wlhrgp!(dm@_e3&a2(q^(6@)-64=^bc#xn^5_6#0rxytl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '24.144.90.143']
 

9d2d7cc794f54128ade2fc0add528e593a275173
SebaLapalma <sebastianlapalma@Sebastians-MacBook-Pro.local>
2024-02-09 10:54:53 -0300

    fixing list error in static files

--- backend/settings.py
+++ backend/settings.py
@@ -192,8 +192,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static_root')]
-MEDIA_ROOT = [os.path.join(BASE_DIR, 'media_root')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 CORS_ALLOW_ALL_ORIGINS = True
 

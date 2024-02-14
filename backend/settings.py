f8ce5c5dce94d66106cb89ec163d84e649b31196
SebaLapalma <sebastianlapalma@Sebastians-MacBook-Pro.local>
2024-02-14 08:21:04 -0300

    adding SECURE_PROXY_SSL_HEADER

--- backend/settings.py
+++ backend/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 
 

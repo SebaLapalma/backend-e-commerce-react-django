58e6dba4996ab3896dd688420cd837989e086ec7
SebaLapalma <sebastianlapalma@Sebastians-MacBook-Pro.local>
2024-02-21 12:40:08 -0300

    changes to admin.py

--- base/admin.py
+++ base/admin.py
@@ -1,9 +1,18 @@
+# admin.py
+
 from django.contrib import admin
-from .models import *
+from .models import Product, Review, Order, OrderItem, ShippingAddress
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_absolute_image_url', 'description', 'price', 'stock', 'createdAt')
+    # ... otros ajustes según tus necesidades ...
 
-admin.site.register(Product)
+    def get_absolute_image_url(self, obj):
+        return obj.get_absolute_image_url()
+
+    get_absolute_image_url.short_description = 'Absolute Image URL'
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Review)
 admin.site.register(Order)
 admin.site.register(OrderItem)

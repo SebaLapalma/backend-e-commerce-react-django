6f7002bb4aa3d74e946daf3a708be69e8300be7d
SebaLapalma <sebastianlapalma@Sebastians-MacBook-Pro.local>
2024-02-22 11:07:39 -0300

    changing static files again

--- backend/settings.py
+++ backend/settings.py
@@ -183,10 +183,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/assets')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/assets/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

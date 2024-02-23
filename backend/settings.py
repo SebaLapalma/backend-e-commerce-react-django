41cc31960fb98be4e13955d5d9a49691f4f56f06
SebaLapalma <sebastianlapalma@Sebastians-MacBook-Pro.local>
2024-02-23 11:10:19 -0300

    modifying static and media directories

--- backend/settings.py
+++ backend/settings.py
@@ -185,8 +185,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/assets')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/assets/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/assets/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
